c2e19de752c4e1be4a5413591b332da15cfdfa43
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-18 10:22:34 -0400

    Activity 3.26

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,10 @@
 import random
 
+MINIMUM = 1
+MAXIMUM = 100
+
 def secret_number():
     '''
     This function generates a random secret number and returns it
     '''
-    return random.randint(1,100)
+    return random.randint(MINIMUM,MAXIMUM)
