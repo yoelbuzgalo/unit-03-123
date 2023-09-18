5f4a48583b0ceeb7e2801b7c38e4feaace8cbba2
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-18 10:31:31 -0400

    Activity 3.27

--- guessing_game.py
+++ guessing_game.py
@@ -3,8 +3,25 @@ import random
 MINIMUM = 1
 MAXIMUM = 100
 
+CORRECT = "Correct"
+TOO_HIGH = "Too High"
+TOO_LOW = "Too Low"
+
 def secret_number():
     '''
     This function generates a random secret number and returns it
     '''
     return random.randint(MINIMUM,MAXIMUM)
+
+def check_guess(number, guess):
+    '''
+    This function compares a number to player's guessed number
+    '''
+    if guess == number:
+        return CORRECT # Returns correct if number matches
+    
+    if guess > number:
+        return TOO_HIGH # Returns too high if guessed number is too high
+    
+    if guess < number:
+        return TOO_LOW # Returns too low if guessed number is too low
--- guessing_game_test.py
+++ guessing_game_test.py
@@ -11,3 +11,42 @@ def test_secret_number():
 
     # Analyze
     assert result == expected
+
+def test_check_number_correct():
+    # Setup
+    random.seed(1)
+    expected = guessing_game.CORRECT
+    secret_num = guessing_game.secret_number()
+    guessed_num = 18
+
+    # Invoke
+    result = guessing_game.check_guess(secret_num, guessed_num)
+
+    # Analyze
+    assert result == expected
+
+def test_check_number_low():
+    # Setup
+    random.seed(1)
+    expected = guessing_game.TOO_LOW
+    secret_num = guessing_game.secret_number()
+    guessed_num = 15
+
+    # Invoke
+    result = guessing_game.check_guess(secret_num, guessed_num)
+
+    # Analyze
+    assert result == expected
+
+def test_check_number_high():
+    # Setup
+    random.seed(1)
+    expected = guessing_game.TOO_HIGH
+    secret_num = guessing_game.secret_number()
+    guessed_num = 23
+
+    # Invoke
+    result = guessing_game.check_guess(secret_num, guessed_num)
+
+    # Analyze
+    assert result == expected
